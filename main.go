9f8d6d3f79b4429de35fd6af4984de724b7f830f
Amir Omidi <amir@aaomidi.com>
2018-10-16 18:11:40 -0400

    Prepare the proper tests.

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 )
 
 func main() {
+	test1()
+	test2()
+}
+
+func test1() {
 	c1 := x.NewCipher([]byte{0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F, 0x77})
 	c2 := x.NewCipher([]byte{0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F, 0x77})
 
@@ -22,4 +27,20 @@ func main() {
 	c2.Apply(decrypted, encrypted)
 
 	fmt.Println(string(decrypted))
+}
+
+func test2() {
+	c := x.NewCipher([]byte{0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F, 0x77})
+
+	fmt.Println("Round 0:")
+	c.PrintReport()
+
+	fmt.Println("Round 100:")
+	c.StepRounds(100)
+	c.PrintReport()
+
+	fmt.Println("Round 1000:")
+	c.StepRounds(900)
+	c.PrintReport()
+
 }
